8fe3c9c24b08b115645837975ab2522b11016b76
moonheeee <enduser0@naver.com>
2020-07-25 20:09:42 +0900

    commit

---
+++ salarymanager.py
@@ -0,0 +1,14 @@
+class Employee :
+    def __init__(self) :
+        self.__number = 0
+        self.__name = 0
+        self.__rank = 0
+        self.__birthday = 0
+        self.__salary = 0
+        self.__joinDate = 0
+
+    def setNumber(self,number) :
+        self.__number = number
+    def setName(self,name) :
+        self.__name = name
+        
