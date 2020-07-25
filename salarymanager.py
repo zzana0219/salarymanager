f20ad4d9fbcee8a5fe679b1a2afe993874e46cef
moonheee <enduser0@naver.com>
2020-07-25 20:52:08 +0900

    Update salarymanager.py
    add get_function and set_function

--- salarymanager.py
+++ salarymanager.py
@@ -1,11 +1,11 @@
 class Employee :
     def __init__(self) :
-        self.__number = 0
-        self.__name = 0
-        self.__rank = 0
-        self.__birthday = 0
-        self.__salary = 0
-        self.__joinDate = 0
+        self.__number = "null"
+        self.__name = "null"
+        self.__rank = "null"
+        self.__birthday = "null"
+        self.__salary = "null"
+        self.__joinDate = "null"
 
     def setNumber(self,number) :
         self.__number = number
@@ -13,4 +13,21 @@ class Employee :
         self.__name = name
     def setRank(self,rank) :
         self.__rank = rank
-        
+    def setBirthday(self, birthday) :
+        self.__birthday = birthday
+    def setSalary(self, salary) :
+        self.__salary = salary
+    def setJoinDate(self, joinDate) :
+        self.__joinDate = joinDate
+    def getNumber(self) :
+        return self.number
+    def getName(self) :
+        return self.__name
+    def getRank(self) :
+        return self.__rank
+    def getBirthday(self) :
+        return self.__birthday
+    def getSalary(self) :
+        return self.__salary
+    def getJoinDate(self) :
+        return self.__joinDate
