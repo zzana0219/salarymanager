7574869bc2944307072497a4900e590c8fd489a1
moonheee <enduser0@naver.com>
2020-07-26 09:23:56 +0900

    Update salarymanager2.py
    파일에 저장이 안돼요...... 다시 불러오지 못해요.....

--- salarymanager2.py
+++ salarymanager2.py
@@ -1,5 +1,4 @@
 from salarymanager import *
-import os
 
 # 파일생성하기
 mkDB()
@@ -8,7 +7,25 @@ mkDB()
 m1 = Manager()
 
 # 로그인
+# 관리자클래스에 로그인하는 로직을 넣어야 하나?
 
+i = 2
 # 1. 사원모드
+if i == 1 :
+    pass
 
 # 2. 관리자모드
+elif i == 2 :
+    j = 2
+    # 2-1.정직원 추가
+    if j == 1 :
+        m1.setFull()
+    # 2-2.정직원 출력?
+    elif j == 2 :
+        print("전체직원을 출력합니다.")
+        arrayF = m1.getFull()
+        for i in arrayF :
+            print(i.getName())
+
+else :
+    print("잘못된 사원번호입니다.")
